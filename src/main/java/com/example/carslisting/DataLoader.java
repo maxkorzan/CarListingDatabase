dc111fd536a3cb7ba65ed5ea6cb46bfdf6a9885b
GBTC440004UR <GBTC440004UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-28 16:28:21 -0500

    EOD commit

--- src/main/java/com/example/carslisting/DataLoader.java
+++ src/main/java/com/example/carslisting/DataLoader.java
@@ -27,6 +27,7 @@ public class DataLoader implements CommandLineRunner {
         car.setName("Ford F-150");
         car.setYear("2020");
         car.setPrice("$35,499");
+        car.setImage("https://res.cloudinary.com/dwsdggfi5/image/upload/v1582924913/Images/vehicle/f150_ur1kc5.jpg");
 
         //create empty set "cars", add "car", and add the set to "category"
         Set<Car> cars = new HashSet<>();
@@ -40,8 +41,6 @@ public class DataLoader implements CommandLineRunner {
 
 //////////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////////////////
-
         category = new Category();
         category.setName("SUV");
 
@@ -49,16 +48,19 @@ public class DataLoader implements CommandLineRunner {
         car1.setName("Land Rover Discovery");
         car1.setYear("2018");
         car1.setPrice("58,399");
+        car1.setImage("https://res.cloudinary.com/dwsdggfi5/image/upload/v1582924913/Images/vehicle/landrover_ehtzmc.jpg");
 
         Car car2 = new Car();
         car2.setName("Porsche Cayenne Turbo S");
         car2.setYear("2019");
         car2.setPrice("$94,899");
+        car2.setImage("https://res.cloudinary.com/dwsdggfi5/image/upload/v1582924913/Images/vehicle/cayenne_lerhuo.jpg");
 
         Car car3 = new Car();
         car3.setName("Mercedes-Benz GLS63 AMG");
         car3.setYear("2019");
         car3.setPrice("$78,299");
+        car3.setImage("https://res.cloudinary.com/dwsdggfi5/image/upload/v1582924913/Images/vehicle/gls_amg_oy9ful.jpg");
 
         cars = new HashSet<>();
         cars.add(car1);
@@ -84,6 +86,7 @@ public class DataLoader implements CommandLineRunner {
         car.setName("Chevrolet Silverado Z71");
         car.setYear("2019");
         car.setPrice("$47,899");
+        car.setImage("https://res.cloudinary.com/dwsdggfi5/image/upload/v1582924913/Images/vehicle/silverado_ehheig.jpg");
 
         cars = new HashSet<>();
         cars.add(car);
@@ -101,6 +104,7 @@ public class DataLoader implements CommandLineRunner {
         car.setName("BMW 335i");
         car.setYear("2020");
         car.setPrice("$37,999");
+        car.setImage("https://res.cloudinary.com/dwsdggfi5/image/upload/v1582924914/Images/vehicle/m3_vmeqbn.jpg");
 
         cars = new HashSet<>();
         cars.add(car);
@@ -118,6 +122,7 @@ public class DataLoader implements CommandLineRunner {
         car.setName("Husqvarna 901 Adventure");
         car.setYear("2022");
         car.setPrice("$18,699");
+        car.setImage("https://res.cloudinary.com/dwsdggfi5/image/upload/v1582924913/Images/vehicle/husqvarna901_bta9pn.jpg");
 
         cars = new HashSet<>();
         cars.add(car);
